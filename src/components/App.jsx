1465447fe19ab3d3e9c439306f4e655863ae4254
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-05-20 10:20:01 +0200

    ⚙️

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+    <div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>  
+    </div>
+}
+
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TabTitle } from "../utils/generalFunction";
+import "../index.css";
+
+
+function Home() {
+  TabTitle("Home");
+  return (
+    <div className="">
+    </div>
+  );
+}
+
+export default Home;
